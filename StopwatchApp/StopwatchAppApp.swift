8ec0fef258d6b701bc6ec1bf95290767484237f1
Grace Gong <ggong@lcs.on.ca>
2022-11-15 22:19:29 -0500

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Grace Gong on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
